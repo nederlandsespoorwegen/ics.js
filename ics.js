fde424d5b3fe279e225e31d5ad62927a16733466
nederlandsespoorwegen <bouw.cms@ns.nl>
2016-12-14 17:08:00 +0100

    RP-6758 - timezone toevoegen

--- ics.js
+++ ics.js
@@ -13,7 +13,25 @@ var ics = function() {
     var calendarEvents = [];
     var calendarStart = [
         'BEGIN:VCALENDAR',
-        'VERSION:2.0'
+        'VERSION:2.0',
+        'BEGIN:VTIMEZONE',
+        'TZID:Europe/Amsterdam',
+        'X-LIC-LOCATION:Europe/Amsterdam',
+        'BEGIN:DAYLIGHT',
+        'TZOFFSETFROM:+0100',
+        'TZOFFSETTO:+0200',
+        'TZNAME:CEST',
+        'DTSTART:19700329T020000',
+        'RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=3',
+        'END:DAYLIGHT',
+        'BEGIN:STANDARD',
+        'TZOFFSETFROM:+0200',
+        'TZOFFSETTO:+0100',
+        'TZNAME:CET',
+        'DTSTART:19701025T030000',
+        'RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=10',
+        'END:STANDARD',
+        'END:VTIMEZONE'
     ].join(SEPARATOR);
     var calendarEnd = SEPARATOR + 'END:VCALENDAR';
 
@@ -86,8 +104,8 @@ var ics = function() {
                 'BEGIN:VEVENT',
                 'CLASS:PUBLIC',
                 'DESCRIPTION:' + description,
-                'DTSTART:' + start,
-                'DTEND:' + end,
+                'DTSTART;TZID=Europe/Amsterdam:' + start,
+                'DTEND;TZID=Europe/Amsterdam:' + end,
                 'LOCATION:' + location,
                 'SUMMARY;LANGUAGE=en-us:' + subject,
                 'END:VEVENT'
